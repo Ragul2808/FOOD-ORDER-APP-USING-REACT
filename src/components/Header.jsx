71c6bcad58a41d9fb68d734668ecc1a77cfb0356
ragul2808 <ragulraja28@gmail.com>
2024-10-02 11:39:50 +0530

    adding for styling purpose for button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import logoImg from '../assets/logo.jpg';
 import React from 'react';
+import Button from './UI/Button';
 
 export default function Header() {
   return (
@@ -9,7 +10,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <button>Cart (0)</button>
+        <Button textOnly >Cart (0)</Button>
       </nav>
     </header>
   );
