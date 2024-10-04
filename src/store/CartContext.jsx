e330c64b7c10961c7fdde96f2b5d3ef416fff26f
ragul2808 <ragulraja28@gmail.com>
2024-10-04 05:50:01 +0530

    adding function for addedItems

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -8,16 +8,38 @@ const CartContext = createContext({
 
 function cartReducer(state, action){
     if (action.type ==="ADD_ITEM") {
-        //
+        const existingCartItemIndex = state.items.findIndex(
+            (item) => item.id === action.item.id
+        );
+
+        const updatedItems = [...state.items];
+
+        if (existingCartItemIndex > -1){
+            const existingItem = state.items[existingCartItemIndex]
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingCartItemIndex.quantity + 1
+
+            }
+            updatedItems[existingCartItemIndex] = updatedItem;
+
+        } else {
+            updatedItems.push({...action.item, quantity: 1});
+
+        }
+        return {...state, items: updatedItems }
     }
-    if (action){
+    
+    if (action.type === 'REMOVE_ITEM'){
 
     }
+
+    return state;
 
 }
 
 export  function CartContextProvider({children}) {
-    useReducer();
+    useReducer(cartReducer, {items: [] });
 
 
     return <CartContext.Provider>{children}</CartContext.Provider>
